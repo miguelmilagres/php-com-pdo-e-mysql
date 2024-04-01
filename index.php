a783b89acbb2e918c1df11aca6a2eb7cdcc95235
miguelmilagres <miguelmdemacedo@gmail.com>
2024-04-01 16:31:51 -0300

    exec method

--- index.php
+++ index.php
@@ -6,6 +6,34 @@ $password = '';
 
 try {
     $connection = new PDO($dsn, $user, $password);
+
+    // criando uma tabela
+    /*
+    $query = 'create table if not exists tb_users (
+        id int not null primary key auto_increment,
+        nome varchar(255) not null,
+        email varchar(255) not null,
+        senha varchar(255) not null
+    )';
+    */
+    ;
+
+    // inserindo dados
+    /* 
+    $query = 'insert into tb_users (
+        nome, email, senha 
+        ) values (
+            "Miguel Milagres", "miguelmilagres@teste.com", "m123456i"
+        )'; 
+    */
+
+    // removendo dados
+    /*
+    $query = 'delete from tb_usuarios';
+    */
+
+    $result = $connection->exec($query);
+
 } catch (PDOException $e) {
     echo "Error " . $e->getCode() . ": " . $e->getMessage();
 }
